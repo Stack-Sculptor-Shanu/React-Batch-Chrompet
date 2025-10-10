807533b692ce346d1f76e1e11d299aac1ca2125c
Amarjeet Kumar <127886134+Stack-Sculptor-Shanu@users.noreply.github.com>
2025-10-10 14:41:22 +0000

    proptypes are not working now

--- prop-types/src/App.jsx
+++ prop-types/src/App.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Child } from "./Child";
 
 const App = () => {
+  let data = "90"
+  let myData = 87
   return (
     <div>
       {/* <Child data={"29"} />
       <Child myData={"Shanu"} /> */}
-      <Child />
+      <Child data = {data} />
+      <Child data = {myData} />
     </div>
   );
 };
